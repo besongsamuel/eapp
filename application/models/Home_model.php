5308b479f92935e82db9eb508e1f8a204f45fae0
besongsamuel <besongsamuel@yahoo.com>
2017-07-04 12:35:26 -0400

    Update Home_model.php
    Removed getLatestProducts method.

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -12,26 +12,9 @@
  */
 class Home_model extends CI_Model 
 {
-	    public function __construct()
-	    {
-		parent::__construct();
-		// Your own constructor code
-	    }
-		
-	public function GetLatestProducts($store_id = -1)
-	{
-		
-		$get = sprintf("%s.*, %s.name, %s.image", STORE_PRODUCT_TABLE, PRODUCT_TABLE, PRODUCT_TABLE);
-		$join = sprintf("%s.product_id = %s.id", STORE_PRODUCT_TABLE, PRODUCT_TABLE);
-		$array = array('period_from <=' => date("Y-m-d"), 'period_to >=' => date("Y-m-d"));
-		if($store_id > -1)
-		{
-			$array['store_id = '] = $store_id;
-		}
-		$this->db->select($get);
-		$this->db->from(STORE_PRODUCT_TABLE);
-		$this->db->join(PRODUCT_TABLE, $join);
-		$this->db->where($array);
-		return $this->db->get()->result();
-	}
+    public function __construct()
+    {
+	parent::__construct();
+	// Your own constructor code
+    }
 }
