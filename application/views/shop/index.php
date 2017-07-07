ad4544ed43993e479308f71398e2238ba21aeea9
besongsamuel <besongsamuel@yahoo.com>
2017-07-07 14:38:13 -0400

    Update index.php

--- application/views/shop/index.php
+++ application/views/shop/index.php
@@ -10,11 +10,26 @@ and open the template in the editor.
 <!-- Animate CSS -->
 <link rel="stylesheet" href="http://<?php echo base_url("assets/css/shop.css")?>">
 
-<div id="shop-container">
+<script>
+	$(document).ready(function()
+  	{
+	  	var scope = angular.element($("#shop-container")).scope();
+        
+		scope.$apply(function()
+		{
+		    scope.base_url = "<?php echo $base_url; ?>";
+		    scope.site_url = "<?php echo $site_url; ?>";
+		    scope.controller = "<?php echo $controller; ?>";
+		    scope.method = "<?php echo $method; ?>";
+		});
+  	});
+</script>
+
+<div id="shop-container" ng-controller="ShopController">
 	
     <md-content layout="column" flex>
 
-     	<md-card id="shopController" ng-include="'../assets/templates/shop-products-table.html'" ng-controller="ShopController"></md-card>
+     	<md-card id="shopController" ng-include="'../assets/templates/shop-products-table.html'"></md-card>
 
     </md-content>
 </div>
