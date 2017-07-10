0c5d7ccc1e4ee09228bf811b0ebec83a76904a94
besongsamuel <besongsamuel@yahoo.com>
2017-07-10 08:42:50 -0400

    Update Geo.php
    Refactored as per codacy

--- application/libraries/Geo.php
+++ application/libraries/Geo.php
@@ -20,26 +20,24 @@ class Geo {
         $street = rtrim($street, ',');
         $address = urlencode($city.','.rtrim($street, ',').','.$state);
         $url = "http://maps.google.com/maps/api/geocode/json?address=$address&sensor=false&region=".trim($country);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        $response = curl_exec($ch);
-        curl_close($ch);
+        $curl_handle = curl_init();
+        curl_setopt($curl_handle, CURLOPT_URL, $url);
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_PROXYPORT, 3128);
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, 0);
+        $response = curl_exec($curl_handle);
+        curl_close($curl_handle);
         $response_a = json_decode($response);
         $status = $response_a->status;
 
-        if ( $status == 'ZERO_RESULTS' )
+        if ( $status != 'ZERO_RESULTS' )
         {
-            return FALSE;
-        }
-        else
-        {
-            $return = array('lat' => $response_a->results[0]->geometry->location->lat, 'long' => $long = $response_a->results[0]->geometry->location->lng);
+            $return = array('lat' => $response_a->results[0]->geometry->location->lat, 'long' => $response_a->results[0]->geometry->location->lng);
             return $return;
         }
+        
+		return FALSE;
     }
     
     /**
