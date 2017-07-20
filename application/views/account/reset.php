db4a158b7afb43bec612bb1ccfeb9a58b6af8a10
Malcom MK <malcom.m@live.ca>
2017-07-20 08:33:45 -0400

    form resset

---
+++ application/views/account/reset.php
@@ -0,0 +1,36 @@
+<div class="container">
+	<div id="passwordreset" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            <div class="panel-title">Réinitialiser mon mot de passe</div>
+                            <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#">Se connecter</a></div>
+                        </div>  
+                        <div class="panel-body" >
+                            <form id="signupform" class="form-horizontal" role="form">
+                                <div class="form-group">
+                                    <label for="reset_email" class="col-md-3 control-label">Adresse email</label>
+                                    <div class="col-md-9">
+                                        <input type="text" class="form-control" id="reset_email" name="email" placeholder="Entrez votre adresse email enregistrée">
+                                    </div>
+                                </div>
+                                    
+                                <div class="form-group">
+                                    <!-- Button -->                                        
+                                    <div class="col-md-offset-3 col-md-9">
+                                        <button id="btn-signup" type="button" class="btn btn-warning">Envoyer</button>
+                                    </div>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <div class="col-md-12 control">
+                                        <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
+                                            Nous vous enverrons un email avec un lien pour réinitialiser votre mot de passe
+                                        </div>
+                                    </div>
+                                </div>    
+                                
+                            </form>
+                         </div>
+                    </div>
+         </div> 
+    </div>
