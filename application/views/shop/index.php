fa5215b0f70b05958f30854994c80606193e8e6a
Samuel Besong <besongsamuel@yahoo.com>
2017-07-20 15:29:14 -0400

    Update index.php
    Remove Menu area!!!

--- application/views/shop/index.php
+++ application/views/shop/index.php
@@ -32,34 +32,6 @@ and open the template in the editor.
   	});
 </script>
 
-    <!-- Begin mainmenu area -->
-    <div class="mainmenu-area" ng-controller="MenuController">
-        <div class="container">
-            <div class="row">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div> 
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li><a href="http://<?php echo site_url("home"); ?>">Accueil</a></li>
-                        <li class="active"><a href="http://<?php echo site_url("shop"); ?>">Magasin</a></li>
-                        <li><a href="http://<?php echo site_url("shop"); ?>">Trouver produit</a></li>
-                        <li><a href="http://<?php echo site_url("cart"); ?>">Cart</a></li>
-                        <li><a href="#">Catégories</a></li>
-                        <li><a href="#">Dépliants</a></li>
-                        <li><a href="#">Contactez nous</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div> 
-    <!-- End mainmenu area -->
-
 <div id="shop-container" ng-controller="ShopController">
 	
     <md-content layout="column" flex>
