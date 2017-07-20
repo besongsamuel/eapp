345d5bb865c1e53fae67f0ffc06a95441932efcb
Samuel Besong <besongsamuel@yahoo.com>
2017-07-20 15:30:24 -0400

    Update menu-controller.js
    cleared menu-controller accordingly

--- assets/js/menu-controller.js
+++ assets/js/menu-controller.js
@@ -4,36 +4,8 @@
  * and open the template in the editor.
  */
 
-angular.module("eappApp").controller("MenuController", ["$rootScope", "$mdDialog", function($rootScope, $mdDialog) 
+angular.module("eappApp").controller("MenuController", ["$rootScope", function($rootScope) 
 {
-    var originatorEv;
-
-    $rootScope.openMenu = function($mdMenu, ev) {
-      originatorEv = ev;
-      $mdMenu.open(ev);
-    };
-
-    $rootScope.notificationsEnabled = true;
-    $rootScope.toggleNotifications = function() {
-      $rootScope.notificationsEnabled = !$rootScope.notificationsEnabled;
-    };
-
-    this.redial = function() {
-      $mdDialog.show(
-        $mdDialog.alert()
-          .targetEvent(originatorEv)
-          .clickOutsideToClose(true)
-          .parent('body')
-          .title('Suddenly, a redial')
-          .textContent('You just called a friend; who told you the most amazing story. Have a cookie!')
-          .ok('That was easy')
-      );
-
-      originatorEv = null;
-    };
-
-    this.checkVoicemail = function() {
-      // This never happens.
-    };
+   
 }]);
 
