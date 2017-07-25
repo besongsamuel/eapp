87cf7b751e3bad2b810bce1a44543b5db17bad85
Samuel Besong <besongsamuel@yahoo.com>
2017-07-25 13:16:53 -0400

    Update create_store_product.php

--- application/views/admin/create_store_product.php
+++ application/views/admin/create_store_product.php
@@ -184,7 +184,7 @@ $(document).ready(function()
         
        
         <!--Select the country and state origin of the product-->
-        <country-state-select country="store_product.country" flag="icons.flag" country-state="store_product.state"></country-state-select>
+        <country-state-select country="store_product.country" flag="flag" country-state="store_product.state"></country-state-select>
         
         <!-- Section to select if product is organic -->        
         <div flex-gt-sm="50">
