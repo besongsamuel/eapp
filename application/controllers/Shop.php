b84e2a5337ca1aac330f76c10d57c6432048d419
Samuel Besong <besongsamuel@yahoo.com>
2017-07-26 12:50:32 -0400

    Update Shop.php

--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -49,6 +49,24 @@ class Shop extends CI_Controller {
         $this->data['body'] = $this->load->view('shop/select_flyer_store', $this->data, TRUE);
         $this->parser->parse('eapp_template', $this->data);
     }
+	
+    public function select_category()
+    {
+        $categories = $this->admin_model->get_all(CATEGORY_TABLE);
+        
+        foreach ($categories as $key => $value) 
+        {
+            $path = ASSETS_DIR_PATH."img/stores/".$value->image;
+            
+            if(!file_exists($path) || empty($value->image))
+            {
+                $categories[$key]->image = "no_image_available.png";
+            }
+        }
+        $this->data['categories'] = addslashes(json_encode($categories));
+        $this->data['body'] = $this->load->view('shop/select_category', $this->data, TRUE);
+        $this->parser->parse('eapp_template', $this->data);
+    }
     
     public function get_store_products()
     {
