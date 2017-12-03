a20df07e8dc448b4f132517b204bfbf7fbd0a5ff
Samuel Besong <besongsamuel@yahoo.com>
2017-12-02 22:01:47 -0500

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -77,7 +77,7 @@ $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => (ENVIRONMENT !== 'production') ? 'root' : 'eapp_user',
-	'password' => (ENVIRONMENT !== 'production') ? '' : 'Shaddy12',
+	'password' => (ENVIRONMENT !== 'production') ? '' : '',
 	'database' => (ENVIRONMENT !== 'production') ? 'eapp' : 'eapp_db',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
