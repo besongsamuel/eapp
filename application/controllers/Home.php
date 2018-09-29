58eceb825a5ab5e2be1990acc1856f9f60e1a9cc
besongsamuel <besongsamuel@localhost>
2018-09-29 19:45:41 -0400

    Changed home page view v1

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -56,9 +56,6 @@ class Home extends CI_Controller {
         
         $this->data["categoryProducts"] = $category_products;
         
-        $this->data['latestProducts'] = $this->home_model->get_store_products_limit(25, 0)["products"];
-
-        
         $this->data['body'] = $this->load->view('home/index', $this->data, TRUE);
         $this->rememberme->recordOrigPage();
         $this->parser->parse('eapp_template', $this->data);
