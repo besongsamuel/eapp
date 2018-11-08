cc33c80f3f43828e03a3da6367ae4721739a6ff2
Check <check.sangare.1@ulaval.ca>
2018-11-07 22:10:56 -0500

    Commit clean changes commit
    Commit clean changes commit

--- application/views/account/select_subscription.php
+++ application/views/account/select_subscription.php
@@ -6,205 +6,222 @@ and open the template in the editor.
 -->
 
 <link rel="stylesheet" href="<?php echo base_url("assets/css/subscription.css")?>">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
 
 <div class="container otiprix-section" layout-margin='30px' ng-controller="SubscriptonController as ctrl">
 
-    <ul class="subacription-ul">
-        <li class="bg-purple">
-            <button>Basique</button>
-        </li>
-        <li class="bg-blue">
-            <button>Standard</button>
-        </li>
-        <li class="bg-blue">
-            <button>Plus</button>
-        </li>
-    </ul>  
+    <ul>
+  <li class="bg-green active">
+    <button>Basique</button>
+  </li>
+  <li class="bg-yellow">
+    <button>Standard</button>
+  </li>
+  <li class="bg-red">
+    <button>Plus</button>
+  </li>
+</ul>  
 
-    <table class="subacription-table">
-        <thead>
-            <tr>
-                <th class="mask"></th>
-                <th class="bg-purple">Basique</th>
-                <th class="bg-blue">Standard</th>
-                <th class="bg-blue">Plus</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Prix par mois</td>
-                <td><span class="txt-top">&dollar; CAD</span><span class="txt-l">0</span></td>
-                <td><span class="txt-top">&dollar; CAD</span><span class="txt-l">99</span></td>
-                <td><span class="txt-top">&dollar; CAD</span><span class="txt-l">199</span></td>
-            </tr>
-            <tr>
-                <td colspan="4" class="sep">Statistiques: Produits plus</td>
-            </tr>
-            <tr>
-                <td>La poportion des produits biologiques ajoutés au panier par les utilisateurs</td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>La catégorie de produits la plus visitée par les utilisateurs</td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits les plus ajoutés au panier par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Origine des produits visités par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits les plus visités par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits qui reviennent le plus souvent en circulaire</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits qui reviennent le plus souvent dans la liste d'épicerie des utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits les plus recherchés par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 marques les plus ajoutés au panier par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Origine des produits ajoutés au panier par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-
-            <tr>
-                <td colspan="4" class="sep">Statistiques: Produits moins</td>
-            </tr>
-            <tr>
-                <td>La catégorie de produits la moins visitée par les utilisateurs</td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits qui reviennent le moins souvent en circulaire</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits les moins visités par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits les moins ajoutés au panier par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits qui reviennent le moins souvent dans la liste d'épicerie des utilisateurs </td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 produits les moins recherchés par les utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Les 5 marques les moins ajoutées au panier par utilisateurs</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td colspan="4" class="sep">Entreprises</td>
-            </tr>
-            <tr>
-                <td>Combien d'utilisateurs visitent mon magasin</td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Combien de produits le magasin le plus visité met en ligne chaque semaine</td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>À quelle distance se trouvent les utilisateurs qui visitent mon magasin </td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Combien d'utilisateurs m'ont choisi comme magasin préféré</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>À quelle distance se trouvent les utilisateurs qui ajoutent mes produits à leur panier</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Quel est le magasin le plus visité</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-            <tr>
-                <td>Quel est le magasin qui a plus de produits dans la liste des produits moins cher</td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tickred">&#x2716;</span></td>
-                <td><span class="tick">&#10004;</span></td>
-            </tr>
-                <tr><td class="mask"></td>
-                <td><md-button class='md-raised md-primary md-hue-2' ng-click="ctrl.selectSubscription(1)">Souscrire</md-button></td>
-                <td>
-                    <div id="dropin-container"></div>
-                    <md-button id="submit-button" class='md-raised md-primary'>Souscrire</md-button>
-                </td>
-                <td>
-                    <div id="dropin-container2"></div>
-                    <md-button id="submit-button2" class='md-raised md-primary'>Souscrire</md-button>
-                </td>
-            </tr> 
-        </tbody>
-    </table>
-  
+<table>
+  <thead>
+    <tr>
+      <th class="text-center">Forfait</th>
+      <th class="bg-green default">Basique</th>
+      <th class="bg-yellow">Standard</th>
+      <th class="bg-red">Plus</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Prix par mois</td>
+      <td class="default"><span class="txt-top">&dollar; CAD</span><span class="txt-l">0</span></td>
+      <td><span class="txt-top">&dollar; CAD</span><span class="txt-l">99</span></td>
+      <td><span class="txt-top">&dollar; CAD</span><span class="txt-l">199</span></td>
+    </tr>
+	
+	<tr>
+      <td colspan="4" class="sep">Produits</td>
+    </tr>
+	<tr>
+      <td>Nombre de produits autorisés</td>
+      <td class="default">30</td>
+      <td>100</td>
+      <td>Illimité</td>
+    </tr>
+	
+	<tr>
+      <td colspan="4" class="sep">Succursale</td>
+    </tr>
+	<tr>
+      <td>Nombre de succursales autorisées</td>
+      <td class="default">1</td>
+      <td>5</td>
+      <td>Illimité</td>
+    </tr>
+	
+    <tr>
+      <td colspan="4" class="sep">Statistiques: Produits plus</td>
+    </tr>
+	<tr>
+      <td>La poportion des produits biologiques ajoutés au panier par les utilisateurs</td>
+      <td class="default"><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>La catégorie de produits la plus visitée par les utilisateurs</td>
+      <td class="default"><span class="tick">&#10004;</span></td>
+	  <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Les 5 produits les plus ajoutés au panier par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Origine des produits visités par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Les 5 produits les plus visités par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Les 5 produits qui reviennent le plus souvent en circulaire</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Les 5 produits qui reviennent le plus souvent dans la liste d'épicerie des utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Les 5 produits les plus recherchés par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Les 5 marques les plus ajoutés au panier par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Origine des produits ajoutés au panier par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	
+    <tr>
+      <td colspan="4" class="sep">Statistiques: Produits moins</td>
+    </tr>
+	<tr>
+      <td>La catégorie de produits la moins visitée par les utilisateurs</td>
+      <td class="default"><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Les 5 produits qui reviennent le moins souvent en circulaire</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Les 5 produits les moins visités par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Les 5 produits les moins ajoutés au panier par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Les 5 produits qui reviennent le moins souvent dans la liste d'épicerie des utilisateurs </td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Les 5 produits les moins recherchés par les utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Les 5 marques les moins ajoutées au panier par utilisateurs</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td colspan="4" class="sep">Entreprises</td>
+    </tr>
+	<tr>
+      <td>Combien d'utilisateurs visitent mon magasin</td>
+      <td class="default"><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Combien de produits le magasin le plus visité met en ligne chaque semaine</td>
+      <td class="default"><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>À quelle distance se trouvent les utilisateurs qui visitent mon magasin </td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Combien d'utilisateurs m'ont choisi comme magasin préféré</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>À quelle distance se trouvent les utilisateurs qui ajoutent mes produits à leur panier</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+	<tr>
+      <td>Quel est le magasin le plus visité</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+      <td>Quel est le magasin qui a plus de produits dans la liste des produits moins cher</td>
+      <td class="default"><span class="tickred">&#x2716;</span></td>
+      <td><span class="tickred">&#x2716;</span></td>
+      <td><span class="tick">&#10004;</span></td>
+    </tr>
+    <tr>
+        <td class="first"></td>
+	<td class="default"><button type="button" class="button first">Souscrire</button></td>
+        <td ><button type="button" class="button second" onclick="window.location.href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=K3PLC2N2CSTL6'">Souscrire</button></td>
+	<td ><button type="button" class="button third" onclick="window.location.href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=67YAR9BVCRBHN'">Souscrire</button></td>
+    </tr> 
+  </tbody>
+</table>
+  <script src="<?php echo base_url("assets/js/angular-components/select-subscription.js")?>"></script>
 </div>
 
 
